08e01d7f5be1b2cc482e45c396fcb6f4492f6967
NULLtm <boseleyowe@tarriers.org>
2020-01-16 17:23:14 -0800

    working on main path
    ppppp

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -2,6 +2,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator
 import com.acmerobotics.roadrunner.path.heading.HeadingInterpolator
+import com.acmerobotics.roadrunner.path.heading.LinearInterpolator
 import com.acmerobotics.roadrunner.path.heading.SplineInterpolator
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
@@ -25,18 +26,18 @@ object TrajectoryGen {
             //.splineTo(Pose2d(50.0, -30.0, 180.0.toRadians))
             //.setReversed(true)
             //.splineTo(Pose2d(10.0, -60.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, 180.0.toRadians))
-            .strafeTo(Vector2d(-44.0, -30.0)).splineTo(Pose2d(0.0, -36.0, 0.0), SplineInterpolator(0.0, 0.0))
-            .splineTo(Pose2d(40.0, -30.0, 0.0), SplineInterpolator(0.0, 0.0))
+            .strafeTo(Vector2d(-44.0, -30.0)).splineTo(Pose2d(0.0, -36.0, 0.0))
+            .splineTo(Pose2d(40.0, -30.0, 0.0))
             .setReversed(true)
-            .splineTo(Pose2d(0.0, -36.0, 0.0), SplineInterpolator(0.0, 0.0))
-            .splineTo(Pose2d(-64.0, -30.0, 0.0), SplineInterpolator(0.0, 0.0))
+            .splineTo(Pose2d(0.0, -36.0, 0.0))
+            .splineTo(Pose2d(-64.0, -30.0, 0.0))
             .setReversed(false)
             .splineTo(Pose2d(0.0, -36.0, 0.0), SplineInterpolator(0.0, 0.0))
             .splineTo(Pose2d(50.0, -30.0, 0.0), SplineInterpolator(0.0, 0.0))
             .lineTo(Vector2d(50.0, -35.0), SplineInterpolator(0.0, (-90.0).toRadians))
             .lineTo(Vector2d(50.0, -30.0), SplineInterpolator((-90.0).toRadians, (-90.0).toRadians))
-            .splineTo(Pose2d(40.0, -60.0, 0.0), SplineInterpolator((-90.0).toRadians, (-90.0).toRadians))
-
+            .lineTo(Vector2d(50.0, -40.0))
+            .splineTo(Pose2d(40.0, -60.0, (180.0).toRadians))
         // weird reversed profiles
         // builder
              //.setReversed(true)
