4879d0b16a1ae6b28ad8ed0e2959b450c7148efb
NULL <doodleddd01@gmail.com>
2021-01-21 08:43:39 -0800

    UPDATE

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -1,54 +1,164 @@
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator
-import com.acmerobotics.roadrunner.path.heading.HeadingInterpolator
-import com.acmerobotics.roadrunner.path.heading.LinearInterpolator
-import com.acmerobotics.roadrunner.path.heading.SplineInterpolator
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
-import javafx.geometry.Pos
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryConstraints
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.PI
 
 object TrajectoryGen {
-    private val constraints = DriveConstraints(60.0, 70.0, 0.0, 270.0.toRadians, 270.0.toRadians, 0.0)
-    private val startPose = Pose2d(10.0, -60.0, 0.0)
+    // Remember to set these constraints to the same values as your DriveConstants.java file in the quickstart
+    private val driveConstraints = DriveConstraints(60.0, 60.0, 0.0, 270.0.toRadians, 270.0.toRadians, 0.0)
+
+    // Remember to set your track width to an estimate of your actual bot to get accurate trajectory profile duration!
+    private const val trackWidth = 16.0
+
+    private val combinedConstraints = MecanumConstraints(driveConstraints, trackWidth)
+
+    private val startPose = Pose2d(-63.0, 15.0, 0.0)
 
     fun createTrajectory(): ArrayList<Trajectory> {
+
         val list = ArrayList<Trajectory>()
 
-        val builder1 = TrajectoryBuilder(Pose2d(-30.0, -60.0, 0.0), constraints)
 
-        // dump routine
+
+        // PATH #1 for closest square
+
+        val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
+
+        val builder2 = TrajectoryBuilder(Pose2d(0.0, 5.0, 0.0), 0.0, combinedConstraints)
+
+        val builder3 = TrajectoryBuilder(Pose2d(0.0, 13.0, 0.0), 0.0, combinedConstraints)
+
+        val builder4 = TrajectoryBuilder(Pose2d(0.0, 21.0, 0.0), 0.0, combinedConstraints)
+
+        val builder5 = TrajectoryBuilder(Pose2d(0.0, 62.0, 0.0), 0.0, combinedConstraints)
+
+        val builder6 = TrajectoryBuilder(Pose2d(14.0, 62.0, 0.0), true, combinedConstraints)
+
+        val builder7 = TrajectoryBuilder(Pose2d(-38.0, 50.0, 270.0.toRadians), 270.0.toRadians, combinedConstraints)
+
+        val builder8 = TrajectoryBuilder(Pose2d(0.0, 36.0, 0.0), 0.0, combinedConstraints)
+
+        val builder9 = TrajectoryBuilder(Pose2d(0.0, 62.0, 0.0), 0.0, combinedConstraints)
+
         builder1
-            //.splineTo(Pose2d(-50.0, -32.0, 180.0.toRadians))
-            //.setReversed(true)
-            //.splineTo(Pose2d(10.0, -60.0, 180.0.toRadians))
-            //.splineTo(Pose2d(50.0, -30.0, 180.0.toRadians))
-            //.setReversed(true)
-            //.splineTo(Pose2d(10.0, -60.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, 180.0.toRadians))
-            .strafeTo(Vector2d(-44.0, -30.0)).splineTo(Pose2d(0.0, -36.0, 0.0))
-            .splineTo(Pose2d(40.0, -30.0, 0.0))
-            .setReversed(true)
-            .splineTo(Pose2d(0.0, -36.0, 0.0))
-            .splineTo(Pose2d(-64.0, -30.0, 0.0))
-            .setReversed(false)
-            .splineTo(Pose2d(0.0, -36.0, 0.0), SplineInterpolator(0.0, 0.0))
-            .splineTo(Pose2d(50.0, -30.0, 0.0), SplineInterpolator(0.0, 0.0))
-            .lineTo(Vector2d(50.0, -35.0), SplineInterpolator(0.0, (-90.0).toRadians))
-            .lineTo(Vector2d(50.0, -30.0), SplineInterpolator((-90.0).toRadians, (-90.0).toRadians))
-            .lineTo(Vector2d(50.0, -40.0))
-            .splineTo(Pose2d(40.0, -60.0, (180.0).toRadians))
-            //.addMarker(40.0, -60.0, (180.0).toRadians)
-        // weird reversed profiles
-        // builder
-             //.setReversed(true)
-             //.splineTo(Pose2d(-12.0, -42.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, startPose.heading))// y = 39 is halfway between the skybridge and partner with 6" on either side
-             //.splineTo(Pose2d(28.0, -42.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, 180.0.toRadians))
-             //.splineTo(Pose2d(48.0, -33.0, -90.0.toRadians), SplineInterpolator(180.0.toRadians, -90.0.toRadians))
-            //.lineTo(Vector2d(48.0, -26.0), ConstantInterpolator(-90.0.toRadians))
+            .forward(39.0)
+            .splineTo(Vector2d(0.0, 5.0), 0.0)
+        builder2
+            .strafeLeft(8.0)
+        builder3
+            .strafeLeft(8.0)
+        builder4
+            .strafeLeft(41.0)
+        builder5
+            .forward(14.0)
+        builder6
+            .splineTo(Vector2d(-26.0, 36.0), 180.0.toRadians)
+            .splineTo(Vector2d(-38.0, 50.0), 90.0.toRadians)
+        builder7
+            .splineTo(Vector2d(-26.0, 36.0), 0.0)
+            .splineTo(Vector2d(0.0, 36.0), 0.0)
+        builder8
+            .strafeLeft(26.0)
+        builder9
+            .forward(10.0)
 
 
-        list.add(builder1.build())
+        // Path #2 for middle square
+
+
+        /*val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
+
+        val builder2 = TrajectoryBuilder(Pose2d(0.0, 5.0, 0.0), 0.0, combinedConstraints)
+
+        val builder3 = TrajectoryBuilder(Pose2d(0.0, 13.0, 0.0), 0.0, combinedConstraints)
+
+        val builder4 = TrajectoryBuilder(Pose2d(0.0, 21.0, 0.0), 0.0, combinedConstraints)
+
+        val builder5 = TrajectoryBuilder(Pose2d(37.0, 39.0, 0.0), true, combinedConstraints)
+
+        val builder6 = TrajectoryBuilder(Pose2d(-38.0, 50.0, 270.0.toRadians), 270.0.toRadians, combinedConstraints)
+
+        val builder7 = TrajectoryBuilder(Pose2d(0.0, 36.0, 0.0), 0.0, combinedConstraints)
+
+        val builder8 = TrajectoryBuilder(Pose2d(34.0, 39.0, 0.0), 0.0, combinedConstraints)
+
+        builder1
+            .forward(39.0)
+            .splineTo(Vector2d(0.0, 5.0), 0.0)
+        builder2
+            .strafeLeft(8.0)
+        builder3
+            .strafeLeft(8.0)
+        builder4
+            .splineTo(Vector2d(37.0, 39.0), 0.0)
+        builder5
+            .splineTo(Vector2d(-26.0, 36.0), 180.0.toRadians)
+            .splineTo(Vector2d(-38.0, 50.0), 90.0.toRadians)
+        builder6
+            .splineTo(Vector2d(-26.0, 36.0), 0.0)
+            .splineTo(Vector2d(0.0, 36.0), 0.0)
+        builder7
+            .splineTo(Vector2d(34.0, 39.0), 0.0)
+        builder8
+            .back(22.0)*/
+
+
+
+        // Path #3 for the farthest square
+
+        /*val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
+
+        val builder2 = TrajectoryBuilder(Pose2d(0.0, 5.0, 0.0), 0.0, combinedConstraints)
+
+        val builder3 = TrajectoryBuilder(Pose2d(0.0, 13.0, 0.0), 0.0, combinedConstraints)
+
+        val builder4 = TrajectoryBuilder(Pose2d(0.0, 21.0, 0.0), 0.0, combinedConstraints)
+
+        val builder5 = TrajectoryBuilder(Pose2d(62.0, 62.0, 0.0), true, combinedConstraints)
+
+        val builder6 = TrajectoryBuilder(Pose2d(-38.0, 50.0, 270.0.toRadians), 270.0.toRadians, combinedConstraints)
+
+        val builder7 = TrajectoryBuilder(Pose2d(0.0, 36.0, 0.0), 0.0, combinedConstraints)
+
+        val builder8 = TrajectoryBuilder(Pose2d(59.0, 62.0, 0.0), 0.0, combinedConstraints)
+
+        builder1
+            .forward(39.0)
+            .splineTo(Vector2d(0.0, 5.0), 0.0)
+        builder2
+            .strafeLeft(8.0)
+        builder3
+            .strafeLeft(8.0)
+        builder4
+            .splineTo(Vector2d(62.0,  62.0), 0.0)
+        builder5
+            .splineTo(Vector2d(-26.0, 36.0), 180.0.toRadians)
+            .splineTo(Vector2d(-38.0, 50.0), 90.0.toRadians)
+        builder6
+            .splineTo(Vector2d(-26.0, 36.0), 0.0)
+            .splineTo(Vector2d(0.0, 36.0), 0.0)
+        builder7
+            .splineTo(Vector2d(59.0, 62.0), 0.0)
+        builder8
+            .back(48.0)*/
+
+
+        list.add(builder1.build());
+        list.add(builder2.build());
+        list.add(builder3.build());
+        list.add(builder4.build());
+        list.add(builder5.build());
+        list.add(builder6.build());
+        list.add(builder7.build());
+        list.add(builder8.build());
+        list.add(builder9.build());
+
         return list
     }
 
